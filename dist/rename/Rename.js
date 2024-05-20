acae35b1550cfcce178d89b166020e56b1173a86
sandarshnaroju <narojusandarsh18@gmail.com>
2024-05-20 12:56:09 +0530

    deleting dist added to build command

--- dist/rename/Rename.js
+++
@@ -1,9 +0,0 @@
-import { runCommand } from "../common.js";
-export const renameAndroidProject = ({ userCommand }) => {
-    const command = `git init && npx react-native-rename ${userCommand}`;
-    console.log(" rename command ", userCommand);
-    const commandRes = runCommand(command);
-    if (!commandRes)
-        process.exit(-1);
-};
-//# sourceMappingURL=Rename.js.map
